a4c1f7f65910e3348dd4a6308c6d459991a8bbc0
Ragh1316 <Ragh1316@gmail.com>
2023-02-10 15:40:30 +0530

    Add project files.

---
+++ eduApp03/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eduApp03.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
